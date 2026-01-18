4ac41b293c6d14fe0ad2b4e9184bdfff90e0a146
YashChoudam <yashchoudam527@gmail.com>
2026-01-18 22:56:03 +0530

    Adding pop_atIndex() function to delete any node present at indext i

--- LINKED LIST/llimplemntaion.cpp
+++ LINKED LIST/llimplemntaion.cpp
@@ -13,13 +13,13 @@ public:
         next = NULL;
     }
 
-    ~Node(){
+    ~Node()
+    {
         if (next != NULL)
         {
-            delete next ;
-            next = NULL ;
+            delete next;
+            next = NULL;
         }
-        
     }
 };
 
@@ -35,13 +35,13 @@ public:
         tail = NULL;
     }
 
-    ~List(){
+    ~List()
+    {
         if (head != NULL)
         {
-            delete head ;
-            head = NULL ;
+            delete head;
+            head = NULL;
         }
-        
     }
 
     void push_front(int val)
@@ -91,26 +91,26 @@ public:
     void insertInMiddle(int val, int position)
     {
         Node *newNode = new Node(val);
-        Node *temp = head ;
+        Node *temp = head;
         if (head == NULL)
         {
             head = tail = newNode;
-            return ;
+            return;
         }
 
         if (position == 0)
         {
-            newNode -> next = head ;
-            head = newNode ;
-            return ;
+            newNode->next = head;
+            head = newNode;
+            return;
         }
-        
+
         for (int i = 0; i < position - 1; i++)
         {
             if (temp == NULL)
             {
                 cout << "Position is Invalid " << endl;
-                delete newNode ;
+                delete newNode;
                 return;
             }
 
@@ -120,18 +120,74 @@ public:
         temp->next = newNode;
     }
 
-    void pop_front(){
+    void pop_front()
+    {
         if (head == NULL)
         {
-            cout << "Linked list is empty " << endl ;
-            return ;
+            cout << "Linked list is empty " << endl;
+            return;
         }
-        
 
-        Node *temp = head ;
-        head = head->next ;
-        temp->next = NULL ;
-        delete temp ;
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    void pop_back()
+    {
+        Node *temp = head;
+        while (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+    }
+
+    void pop_middle(int position)
+    {
+        if (head == NULL)
+        {
+            cout << "Empty List" << endl;
+            return;
+        }
+
+        if (position == 0)
+        {
+            Node *temp = head;
+            head = head->next;
+            temp->next = NULL;
+            delete temp;
+
+            if (head == NULL)
+                tail = NULL;
+            return;
+        }
+
+        Node *previous = head;
+        for (int i = 0; i < position - 1; i++)
+        {
+            if (previous->next == NULL)
+            {
+                cout << "Position does not exist" << endl;
+                return;
+            }
+            previous = previous->next;
+        }
+
+        Node *toDelete = previous->next;
+        if (toDelete == NULL)
+            return;
+
+        previous->next = toDelete->next;
+
+        if (toDelete == tail)
+            tail = previous;
+
+        toDelete->next = NULL;
+        delete toDelete;
     }
 };
 
@@ -146,10 +202,11 @@ int main()
     // ll.push_Back(4);
     // ll.push_Back(5);
     // ll.push_Back(6);
-    ll.insertInMiddle(100,2);
+    ll.insertInMiddle(100, 2);
     ll.printLL();
-    ll.pop_front();
-    cout << endl ;
+    // ll.pop_back();
+    ll.pop_middle(1);
+    cout << endl;
     ll.printLL();
     return 0;
 }
