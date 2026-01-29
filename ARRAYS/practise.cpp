7470b6726abd583379c726f18cad7d136e641ca2
YashChoudam <yashchoudam527@gmail.com>
2026-01-29 18:38:04 +0530

    practise

--- ARRAYS/practise.cpp
+++ ARRAYS/practise.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
-void accept(int arr[], int n)
-{
-    cout << "Enter the elements of the array : ";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    cout << endl;
-}
+
 void display(int arr[], int n)
 {
-    cout << "Array : ";
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << "  ";
     }
     cout << endl;
 }
-
+void accept(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << endl;
+}
 int binarySearch(int arr[], int n, int key)
 {
     int start = 0, end = n - 1;
-
     while (start < end)
     {
         int mid = start + (end - start) / 2;
@@ -39,17 +36,14 @@ int binarySearch(int arr[], int n, int key)
             start = mid + 1;
         }
     }
-    return -1 ;
 }
-
 int main()
 {
     int n;
-    cout << "Enter the total number of elements in the array";
+    cout << "Enter the numeber of elements in the array ";
     cin >> n;
     int A[n];
     accept(A, n);
+    cout << "Array : ";
     display(A, n);
-    cout << binarySearch(A,n,5);
-    return 0 ;
 }
